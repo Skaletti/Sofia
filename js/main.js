0209541f169b55e0412292e9b5f9a69bc28b6ad9
Skaletti <98595208+Skaletti@users.noreply.github.com>
2022-03-13 17:35:00 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.design-slider').slick({
+    dots: false,
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt="">',
+    nextArrow: '<img class="arrow arrow-right" src="images/arrow-right.svg" alt="">',
+    });
+});
